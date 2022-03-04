d7c9d54e91f9af69589a8c0a3928b97e72e0b62a
Fred Lee <lfredl109@gmail.com>
2022-03-03 23:46:35 -0800

    working

---
+++ pages/index.tsx
@@ -0,0 +1,76 @@
+import Layout from "../components/MyLayout";
+import Link from "next/link";
+import fetch from "isomorphic-unfetch";
+
+function ShowLink({ show }) {
+  return (
+    <li key={show.id}>
+      <Link as={`/p/${show.id}`} href={`/post?id=${show.id}`}>
+        <a>{show.name}</a>
+      </Link>
+      <style jsx>{`
+        li {
+          list-style: none;
+          margin: 5px 0;
+        }
+
+        a {
+          text-decoration: none;
+          color: blue;
+        }
+
+        a:hover {
+          opacity: 0.6;
+        }
+      `}</style>
+    </li>
+  );
+}
+
+export default function Index(props) {
+  return (
+    <Layout>
+      <h1>Batman TV Shows</h1>
+      <ul>
+        {props.shows.map(({ show }) => (
+          <ShowLink key={show.id} show={show} />
+        ))}
+      </ul>
+      <style jsx>{`
+        h1,
+        a {
+          font-family: "Arial";
+        }
+
+        ul {
+          padding: 0;
+        }
+
+        li {
+          list-style: none;
+          margin: 5px 0;
+        }
+
+        a {
+          text-decoration: none;
+          color: blue;
+        }
+
+        a:hover {
+          opacity: 0.6;
+        }
+      `}</style>
+    </Layout>
+  );
+}
+
+Index.getInitialProps = async function () {
+  const res = await fetch("https://api.tvmaze.com/search/shows?q=batman");
+  const data = await res.json();
+
+  console.log(`Show data fetched. Count: ${data.length}`);
+
+  return {
+    shows: data,
+  };
+};
