bd77cfe544fa47e022778f1060a81fe5f46b8b2e
Fred Lee <lfredl109@gmail.com>
2022-03-10 00:30:00 -0800

    Updates:
    1. onboard to ESlint, combined with prettier
    2. use local cache for rAPI client

--- pages/api/hours/[region]/[id].ts
+++ pages/api/hours/[region]/[id].ts
@@ -1,9 +1,25 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { RiotAPI, RiotAPITypes, PlatformId } from "@fightmegg/riot-api";
 import type { NextApiRequest, NextApiResponse } from "next";
 import "dotenv/config";
 
+console.log("process.env.NODE_ENV", process.env.NODE_ENV);
+const rAPI = new RiotAPI(process.env.RIOT_API_KEY!, {
+  debug: process.env.NODE_ENV === "development",
+  cache: {
+    cacheType: "local",
+    ttls: {
+      byMethod: {
+        [RiotAPITypes.METHOD_KEY.MATCH_V5.GET_MATCH_BY_ID]: 1000 * 60 * 10, // 10min in ms
+        [RiotAPITypes.METHOD_KEY.SUMMONER.GET_BY_SUMMONER_NAME]: 1000 * 60 * 10, // 10min in ms
+        [RiotAPITypes.METHOD_KEY.MATCH_V5.GET_IDS_BY_PUUID]: 1000 * 60 * 10, // 10min in ms
+      },
+    },
+  },
+});
+
 type ResponseData = {
+  matchesComputed?: number;
+  duration?: number;
   data?: RiotAPITypes.MatchV5.MatchDTO[];
 };
 
@@ -12,34 +28,74 @@ export default async function handler(
   res: NextApiResponse<ResponseData>
 ) {
   try {
-    const platform: RiotAPITypes.LoLRegion = req.query
-      .request as unknown as RiotAPITypes.LoLRegion;
-    const rAPI = new RiotAPI(process.env.RIOT_API_KEY!);
+    let quota: number = parseInt(process.env.QUOTA as string);
+    const { region, cluster } = getRegion(req.query.region as string);
+
+    // https://developer.riotgames.com/apis#match-v5
     const summoner: RiotAPITypes.Summoner.SummonerDTO =
       await rAPI.summoner.getBySummonerName({
-        // region: platform,
-        region: PlatformId.NA1,
+        region: region,
         summonerName: req.query.id as string,
       });
+    quota--;
+
+    // https://developer.riotgames.com/apis#match-v5
     const matches: string[] = await rAPI.matchV5.getIdsbyPuuid({
-      cluster: PlatformId.AMERICAS,
+      cluster: cluster,
       puuid: summoner.puuid,
       params: {
-        count: 5,
+        count: 100, // 100 is maximum.
       },
     });
+    quota--;
 
-    const matchDtos: RiotAPITypes.MatchV5.MatchDTO[] = await Promise.all(
-      matches.map(async (matchId) => {
-        return await rAPI.matchV5.getMatchById({
-          cluster: PlatformId.AMERICAS,
-          matchId: matchId,
-        });
-      })
+    let firstGameTime: Date;
+    let matchesComputed = 0;
+    let duration = 0;
+    await Promise.all(
+      matches
+        .filter((_match, index) => index < quota)
+        .map(async (matchId, index, { length }) => {
+          matchesComputed++;
+          const res: RiotAPITypes.MatchV5.MatchDTO =
+            await rAPI.matchV5.getMatchById({
+              cluster: cluster,
+              matchId: matchId,
+            });
+          console.log("FRED creation", index, new Date(res.info.gameCreation));
+          duration += res.info.gameDuration;
+          if (index + 1 === length) {
+            firstGameTime = new Date(res.info.gameCreation);
+            console.log("FRED FOUND IT", new Date(res.info.gameCreation));
+          }
+        })
     );
-    res.status(200).send({ data: matchDtos });
+    console.log("FRED firstGameTime", firstGameTime!);
+
+    res
+      .status(200)
+      .send({ matchesComputed: matchesComputed, duration: duration });
   } catch (error) {
     console.error("api.ts ERROR: ", error, typeof error);
     res.status(500).send({});
+  }
+}
+
+function getRegion(region: string): {
+  region: RiotAPITypes.LoLRegion;
+  cluster: RiotAPITypes.Cluster;
+} {
+  switch (region.toLowerCase()) {
+    case PlatformId.NA1:
+      return {
+        region: PlatformId.NA1,
+        cluster: PlatformId.AMERICAS,
+      };
+    default:
+      //TODO: Handle other region cases
+      return {
+        region: PlatformId.NA1,
+        cluster: PlatformId.AMERICAS,
+      };
   }
 }
