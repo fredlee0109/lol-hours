c60c238cf0b23a80fe2af45661a3a422967d621b
Fred Lee <lfredl109@gmail.com>
2022-03-15 21:51:15 -0700

    Making search bar look better

--- components/Search.tsx
+++ components/Search.tsx
@@ -1,20 +1,24 @@
-import classNames from "classnames";
 import Image from "next/image";
 
 export default function Search() {
   return (
     <div className="fixed h-screen w-screen flex items-center justify-center bg-indigo-300">
-      <div className="w-96 -mt-60">
-        <form className="rounded-sm">
+      <div className="w-[40rem] -mt-60">
+        <form className="relative">
           <input
-            className="h-12 w-full"
+            className="rounded-sm px-5 h-12 w-full focus:outline-none 
+            border border-slate-300 focus:border-sky-500"
             type="text"
             name="Summoner"
             id="Summoner"
             placeholder="Summon Name"
+            autoComplete="off"
           />
-          <button className="h-12 absolute bg-indigo-600" type="submit">
-            <Image src="/icon-gg-white.svg" alt=".GG" height={48} width={100} />
+          <button
+            className="h-10 w-20 mt-1 absolute bg-indigo-600 rounded-md left-[34.5rem]"
+            type="submit"
+          >
+            <Image src="/icon-gg-white.svg" alt=".GG" height={40} width={50} />
           </button>
         </form>
       </div>
